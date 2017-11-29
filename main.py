e7c7b4adf915da235fb87398d259c8d18a689def
Phil R. Larochelle <phill@abracontrols.local>
2017-11-29 10:18:12 -0700

    Add window color support and resizing, show values in CAD

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import sched, time, click, requests, json, re, datetime, sys
+import sched, time, click, requests, json, re, datetime, sys, os
 
 from terminaltables import SingleTable
 
@@ -8,10 +8,17 @@ from ascii_graph.colordata import vcolor
 
 from dateutil.relativedelta import relativedelta
 
-def resize_terminal(rows, cols):
-  sys.stdout.write('\x1b[8;{rows};{cols}t'.format(rows=rows, cols=cols))
+from colorama import init
+# windows color support
+init()
 
-url = 'https://api.coinmarketcap.com/v1/ticker/?limit=100'
+def resize_terminal(rows, cols):
+  # linux
+  sys.stdout.write('\x1b[8;{rows};{cols}t'.format(rows=rows, cols=cols))
+  # windows
+  os.system('mode con: cols={cols} lines={rows}'.format(rows=rows, cols=cols))
+
+url = 'https://api.coinmarketcap.com/v1/ticker/?convert=CAD&limit=0'
 
 def fetch_data(url):
   res = requests.get(url)
@@ -78,7 +85,7 @@ def make_table(data, portfolio):
       # Header
       click.style('Rank', fg='white'),
       click.style('Coin', fg='white'),
-      click.style('USD Price', fg='white'),
+      click.style('CAD Price', fg='white'),
       click.style('Coins Owned', fg='white'),
       click.style('Net Worth', fg='white'),
       click.style('24 Hour Volume', fg='white'),
@@ -95,11 +102,11 @@ def make_table(data, portfolio):
     table_row = [
       click.style(row['rank'], fg='white'),
       click.style(id, fg='cyan'),
-      click.style('$' + add_commas(row['price_usd']), fg='green'),
+      click.style('$' + add_commas(row['price_cad']), fg='green'),
       click.style(add_commas(portfolio[id]), fg='green'),
-      click.style('$' + add_commas(str(int(round(float(portfolio[id]) * float(row['price_usd']),)))), fg='green'),
-      click.style('$' + add_commas(format(float(row['24h_volume_usd']), '.2f')), fg='green'),
-      click.style('$' + add_commas(format(float(row['market_cap_usd']), '.2f')), fg='green'),
+      click.style('$' + add_commas(str(int(round(float(portfolio[id]) * float(row['price_cad']),)))), fg='green'),
+      click.style('$' + add_commas(format(float(row['24h_volume_cad']), '.2f')), fg='green'),
+      click.style('$' + add_commas(format(float(row['market_cap_cad']), '.2f')), fg='green'),
       style_percent(row['percent_change_1h']),
       style_percent(row['percent_change_24h']),
       style_percent(row['percent_change_7d']),
@@ -121,7 +128,7 @@ def make_graph(data, portfolio):
     id = row['id']
     if (len(id) > longest):
       longest = len(id)
-    value = float(portfolio[id]) * float(row['price_usd'])
+    value = float(portfolio[id]) * float(row['price_cad'])
     bar_data[id] = value
     portfolio_total += value
 
@@ -154,7 +161,7 @@ def make_graph(data, portfolio):
 
 def pretty_total(total):
   label = click.style(
-    'Portfolio Total: ',
+    'Portfolio CAD Total: ',
     fg='magenta'
   )
   dollars = click.style(
